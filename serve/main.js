5c4cb3a54e5d2e97d0b4a3e35e03b115dd76f60a
Matthew Martori <matthew.martori@gmail.com>
2019-09-19 18:41:23 -0400

    remove worker

--- serve/main.js
+++ serve/main.js
@@ -1,13 +1,9 @@
 let express = require('express')
-let open = require("open");
 let app = express()
 
 app.listen(3000)
 
 console.log("server listening on port 3000")
 
-app.use('/', (req, res, next) => {
-  express.static("..")(req, res, next);
-})
-
-open("http://localhost:3000");
+app.use(express.static('..'))
+// open("http://localhost:3000");
